8ef2b6e52a69a567de58f8d064b3f7fd10140a29
Ruiyot Abby <ruiyotabby@gmail.com>
2023-10-09 11:54:38 +0300

    Add resources to useResource hook

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,15 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then((response) => setResources(response.data))
+      .catch((error) => (error))
+  }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
@@ -45,7 +50,7 @@ const App = () => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
